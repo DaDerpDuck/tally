23ff8e9ae084d629f67804ca717ec8b26f80431f
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2026-08-24 21:44:52 +0300

    Use descriptor source data typing

--- src/state/descriptor/Descriptor.ts
+++ src/state/descriptor/Descriptor.ts
@@ -1,21 +1,20 @@
-import type { Source } from "../source/Source.js";
 import type { DescriptorType, ExtractDataFromDescriptor } from "./DescriptorType.js";
 
 type Disconnect = () => void;
 
 export interface AnyDescriptor {
 	readonly id: number;
-	readonly type: DescriptorType<unknown, Source>;
+	readonly type: DescriptorType<unknown, unknown>;
 
-    set(data: unknown): void;
-    get(): unknown;
+	set(data: unknown): void;
+	get(): unknown;
 	onUpdate(callback: (self: this) => void): Disconnect;
 	onDestroy(callback: (self: this) => void): Disconnect;
 	destroy(): void;
 }
 
 export interface Descriptor<
-	TDescriptorType extends DescriptorType<unknown, Source>,
+	TDescriptorType extends DescriptorType<unknown, unknown>,
 > extends AnyDescriptor {
 	readonly id: number;
 	readonly type: TDescriptorType;
